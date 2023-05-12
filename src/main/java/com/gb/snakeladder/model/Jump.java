e0b01574ae198f3561a22741232569e5f0891636
Anshul Kapoor <anshul.kapoor@olx.com>
2023-05-12 21:19:14 +0530

    Game SL

---
+++ src/main/java/com/gb/snakeladder/model/Jump.java
@@ -0,0 +1,13 @@
+package com.gb.snakeladder.model;
+
+import lombok.Data;
+
+/**
+ * It can replace snakes and ladders as individual classes because they have same params
+ */
+@Data
+public class Jump {
+
+    private int start;
+    private int end;
+}
